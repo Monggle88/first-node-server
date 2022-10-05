9f8416a6eae4685496c72d598fb40916547dbd42
Monggle88 <fmonggle88@gmail.com>
2022-10-06 03:56:40 +0900

    API 기초

--- routes/comments.js
+++ routes/comments.js
@@ -3,11 +3,23 @@ import commentSchema from '../schemas/comment.js';
 const router = express.Router();
 
 // 댓글 작성
+router.post('/:_postId', (req, res, next) => {
+    res.status(201).json({ message: '댓글 작성 성공' });
+});
 
-// 게시글 id로 댓글 목록 가져오기
+// 댓글 목록 가져오기
+router.get('/:_postId', (req, res, next) => {
+    res.status(200).json({ message: '댓글 목록 불러오기' });
+});
 
 // 댓글 수정하기
+router.put('/:_commentId', (req, res, next) => {
+    res.status(201).json({ message: '댓글 수정 성공' });
+});
 
 // 댓글 삭제
+router.delete('/:_commentId', (req, res, next) => {
+    res.sendStatus(204);
+});
 
 export default router;
