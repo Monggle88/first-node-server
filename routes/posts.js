aa04ab9354444dd55f2294251039e9079da47de7
Monggle88 <fmonggle88@gmail.com>
2022-10-06 05:09:38 +0900

    posts 작성

--- routes/posts.js
+++ routes/posts.js
@@ -3,28 +3,99 @@ import postSchema from '../schemas/post.js';
 const router = express.Router();
 
 // 게시글 목록 조회 (내림차순)
-router.get('/', (req, res, next) => {
-    res.status(200).json({ message: '게시글 목록 불러오기' });
+router.get('/', async (req, res, next) => {
+    try {
+        const posts = await postSchema.find().sort({ createdAt: 'desc' }); // 내림차순
+        const post = posts.map((post) => {
+            // 필요한 값만 찾아 post에 할당
+            return {
+                postId: post._id,
+                user: post.user,
+                title: post.title,
+                createdAt: post.createdAt,
+            };
+        });
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(400).json({ message: '게시글 목록 조회 오류' });
+    }
 });
 
 // 게시글 작성
-router.post('/', (req, res, next) => {
-    res.status(201).json({ message: '게시글 작성 성공' });
+router.post('/', async (req, res, next) => {
+    try {
+        const { user, password, title, content } = req.body;
+        await postSchema.create({ user, password, title, content });
+        res.status(201).json({ message: '게시글 작성 성공' });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: 'Error! 게시글 작성 실패' });
+    }
 });
 
 // 게시글 상세 조회
-router.get('/:_postId', (req, res, next) => {
-    res.status(200).json({ message: '게시글 상세 조회' });
+router.get('/:_postId', async (req, res, next) => {
+    try {
+        const targetId = req.params._postId;
+        const post = await postSchema.findOne({ _id: targetId });
+        const targetPost = {
+            postId: post._id,
+            user: post.user,
+            title: post.title,
+            content: post.content,
+        };
+        res.status(200).json(targetPost);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: 'Error! 게시글 상세 조회 실패' });
+    }
 });
 
 // 게시글 수정 (비밀번호 확인)
-router.put('/:_postId', (req, res, next) => {
-    res.status(201).json({ message: '게시글 수정 성공' });
+router.put('/:_postId', async (req, res, next) => {
+    try {
+        const targetId = req.params._postId;
+        const { password, title, content } = req.body;
+        const targetPost = await postSchema.findOne({ _id: targetId });
+        if (targetPost.password === password) {
+            await postSchema.updateOne(
+                { _id: targetId },
+                { $set: { title, content } },
+            );
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: '비밀번호가 일치하지 않습니다.',
+            });
+        }
+        res.status(201).json({
+            message: '게시글이 성공적으로 수정되었습니다.',
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: 'Error! 게시글 수정 실패' });
+    }
 });
 
 // 게시글 삭제 (비밀번호 확인)
-router.delete('/:_postId', (req, res, next) => {
-    res.sendStatus(204);
+router.delete('/:_postId', async (req, res, next) => {
+    try {
+        const targetId = req.params._postId;
+        const inputPassword = req.body.password;
+        const targetPost = await postSchema.findOne({ _id: targetId });
+        if (targetPost.password === inputPassword) {
+            await postSchema.deleteOne({ _id: targetId });
+        } else {
+            return res
+                .status(400)
+                .json({ success: false, message: 'Error! 비밀번호 불일치.' });
+        }
+        res.status(201).json({
+            message: '게시글이 성공적으로 삭제되었습니다.',
+        });
+    } catch (error) {
+        res.status(400).json({ message: 'Error! 게시글 삭제 실패' });
+    }
 });
 
 export default router;
